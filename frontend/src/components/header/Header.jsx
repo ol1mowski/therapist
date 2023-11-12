aa940dff7fa275c07d1207c4e0745c1512a36337
Ol1mowski <olimarki123@wp.pl>
2023-11-12 20:33:04 +0100

    splice header file

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import style from "./header.module.scss";
 import hamburger from "../../assets/icons/hamburger.svg";
 import hamburger_x from "../../assets/icons/hamburger_x.svg";
 import { useEffect, useRef } from "react";
+import Navbar from "./Navbar/Navbar";
 
 const Header = () => {
   const hamburgerBtn = useRef(null);
@@ -37,6 +38,13 @@ const Header = () => {
   return (
     <>
       <header className={style.headerContainer}>
+
+        <Navbar />
+
+
+
+        {/* hamburger menu  */}
+
         <nav className={style.headerContainer__hamburgerNav}>
           <div className={style.headerContainer__hamburgerNav__title}>
             <h2 className={style.headerContainer__hamburgerNav__title__h2}>
@@ -49,53 +57,6 @@ const Header = () => {
           >
             <img src={hamburger} alt="hamburger icon" />
           </div>
-        </nav>
-
-        <nav className={style.headerContainer__navbar}>
-          <section className={style.headerContainer__navbar__navigation}>
-            <div
-              className={style.headerContainer__navbar__navigation__item}
-            >
-              <a href="#">Home</a>
-            </div>
-            <div
-              className={style.headerContainer__navbar__navigation__item}
-            >
-              <a href="#">About</a>
-            </div>
-            <div
-              className={style.headerContainer__navbar__navigation__item}
-            >
-              <a href="#">Docs</a>
-            </div>
-            <div
-              className={style.headerContainer__navbar__navigation__item}
-            >
-              <a href="#">Technologies</a>
-            </div>
-          </section>
-
-          <section className={style.headerContainer__navbar__action}>
-            <div
-              className={style.headerContainer__navbar__action__item}
-            >
-              <a
-                href="#"
-              >
-                Login
-              </a>
-            </div>
-            <div
-              className={style.headerContainer__navbar__action__item}
-            >
-              <a
-                href="#"
-              >
-                Try
-              </a>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="tabler-icon tabler-icon-external-link"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg>
-            </div>
-          </section>
         </nav>
 
         <nav ref={navSection} className={style.headerContainer__navSection}>
