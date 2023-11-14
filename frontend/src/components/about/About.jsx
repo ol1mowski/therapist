bc6151d9c2a3e8aecb30135001ed9437762b6a76
Ol1mowski <olimarki123@wp.pl>
2023-11-14 21:01:15 +0100

    implement rwd to mobile

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -18,6 +18,18 @@ const About = () => {
                         </a>
                     </div>
                 </section>
+                <section className={style.aboutContainer__header__content}>
+                    <article className={style.aboutContainer__header__content__text}>
+                        <p className={style.aboutContainer__header__content__text__p}>
+                            In response to the challenges, a team of teachers, psychologists and engineers decided to create an innovative solution. They decided to create a virtual therapist who could help students understand and deal with their emotional problems. This is how the idea of "<span className={style.aboutContainer__span}>Alice</span>" was born.
+                        </p>
+                    </article>
+                    <article className={style.aboutContainer__header__content__text}>
+                        <p className={style.aboutContainer__header__content__text__p}>
+                            The story of <span className={style.aboutContainer__span}>Alice</span>'s creation is a perfect example of how advanced technology and a commitment to student well-being can come together to create innovative solutions in the fields of education and psychology.
+                        </p>
+                    </article>
+                </section>
             </section>
         </>
     );
