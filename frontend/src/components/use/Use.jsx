8ffa05f93799a0cc44ca5fe9fe16d176b5221726
Ol1mowski <olimarki123@wp.pl>
2023-11-15 20:26:12 +0100

    improve UI in mobile version [use section]

--- frontend/src/components/use/Use.jsx
+++ frontend/src/components/use/Use.jsx
@@ -19,12 +19,14 @@ const Use = () => {
                         <div className={style.useContainer__questionsPart__wrapper__question}>
                             <span className={style.useContainer__boldSpan}>Tell ?</span>
                             <p className={style.useContainer__questionsPart__wrapper__question__p}>
+                                <svg className={style.useContainer__svg} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>{` .svg { fill: #2C2F32; } `}</style><path d="M205 34.8c11.5 5.1 19 16.6 19 29.2v64H336c97.2 0 176 78.8 176 176c0 113.3-81.5 163.9-100.2 174.1c-2.5 1.4-5.3 1.9-8.1 1.9c-10.9 0-19.7-8.9-19.7-19.7c0-7.5 4.3-14.4 9.8-19.5c9.4-8.8 22.2-26.4 22.2-56.7c0-53-43-96-96-96H224v64c0 12.6-7.4 24.1-19 29.2s-25 3-34.4-5.4l-160-144C3.9 225.7 0 217.1 0 208s3.9-17.7 10.6-23.8l160-144c9.4-8.5 22.9-10.6 34.4-5.4z" /></svg>
                                 Yes, <span className={style.useContainer__aliceSpan}>Alice</span> understand a human speech!
                             </p>
                         </div>
                         <div className={style.useContainer__questionsPart__wrapper__question}>
                             <span className={style.useContainer__boldSpan}>How it’s possible ?</span>
                             <p className={style.useContainer__questionsPart__wrapper__question__p}>
+                                <svg className={style.useContainer__svg} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>{` .svg { fill: #2C2F32; } `}</style><path d="M205 34.8c11.5 5.1 19 16.6 19 29.2v64H336c97.2 0 176 78.8 176 176c0 113.3-81.5 163.9-100.2 174.1c-2.5 1.4-5.3 1.9-8.1 1.9c-10.9 0-19.7-8.9-19.7-19.7c0-7.5 4.3-14.4 9.8-19.5c9.4-8.8 22.2-26.4 22.2-56.7c0-53-43-96-96-96H224v64c0 12.6-7.4 24.1-19 29.2s-25 3-34.4-5.4l-160-144C3.9 225.7 0 217.1 0 208s3.9-17.7 10.6-23.8l160-144c9.4-8.5 22.9-10.6 34.4-5.4z" /></svg>
                                 <span className={style.useContainer__aliceSpan}>Alice</span> has AI and she can talking with you!
                             </p>
                         </div>
