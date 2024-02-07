66e43620c06313bdb0079a877649a0199c33042d
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-07 10:58:09 +0100

    [DashboardHeader] new component: add userName component

--- frontend/src/Pages/DashboardPage/Dashboard-Header/DashboardHeader.tsx
+++ frontend/src/Pages/DashboardPage/Dashboard-Header/DashboardHeader.tsx
@@ -3,7 +3,9 @@ import s from '../Dashboard-style/Dashboard.module.scss';
 export const DashboardHeader = () => {
   return (
     <header className={s.headerContainer}>
-
+        <section className={s.headerContainer__userName}>
+          Hi, Oliwier !
+        </section>
     </header>
   )
 }
