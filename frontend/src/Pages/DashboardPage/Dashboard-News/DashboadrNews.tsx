e897b4252a97cbfe1ccc33f7e373d2e5cb55835e
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-07 13:26:19 +0100

    [DashboardNews] add: add new Component files

---
+++ frontend/src/Pages/DashboardPage/Dashboard-News/DashboadrNews.tsx
@@ -0,0 +1,11 @@
+import s from '../Dashboard-style/Dashboard.module.scss';
+
+const DashboadrNews = () => {
+  return (
+    <section className={s.newsContainer}>
+
+    </section>
+  );
+};
+
+export default DashboadrNews;
