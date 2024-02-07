5e7e5c3d5cd9de572d7e7b92b84cb869c187b9c1
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-07 11:16:47 +0100

    [DashboardHeader] style: add full RWd to component

--- frontend/src/Pages/DashboardPage/Dashboard-Header/DashboardHeader.tsx
+++ frontend/src/Pages/DashboardPage/Dashboard-Header/DashboardHeader.tsx
@@ -1,11 +1,28 @@
-import s from '../Dashboard-style/Dashboard.module.scss';
+import s from "../Dashboard-style/Dashboard.module.scss";
 
 export const DashboardHeader = () => {
   return (
     <header className={s.headerContainer}>
-        <section className={s.headerContainer__userName}>
-          Hi, Oliwier !
+      <section className={s.headerContainer__userName}>Hi, User Name !</section>
+      <section>
+        <section className={s.headerContainer__projectTitle}>
+          <h2><span className={s.alice}>Alice</span> Virtual AI Therapist</h2>
         </section>
+      </section>
+      <section className={s.headerContainer__userAccount}>
+        <div className={s.headerContainer__userAccount__icon}>
+          <img
+            width="50"
+            height="50"
+            src="https://img.icons8.com/ios/50/ffffff/user-male-circle--v1.png"
+            alt="user-male-circle--v1"
+          />
+        </div>
+
+        <div className={s.headerContainer__userAccount__account}>
+          <h3>Your Account</h3>
+        </div>
+      </section>
     </header>
-  )
-}
+  );
+};
