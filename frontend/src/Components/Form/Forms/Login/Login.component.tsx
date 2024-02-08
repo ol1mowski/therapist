3b69e964ad8e0eab50a57039fdfc9c155433b734
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-08 20:47:43 +0100

    [LoginCompoent] refactoring: introduce uncontrolled component pattern

--- frontend/src/Components/Form/Forms/Login/Login.component.tsx
+++ frontend/src/Components/Form/Forms/Login/Login.component.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef, useState, MouseEvent } from "react";
+import { useEffect, useRef, useState, type MouseEvent } from "react";
 import { Navigate } from "react-router-dom";
-import { FormWrapper } from "../../FormWrapper-component/FormWrapper.component";
-import s from "../../Form-sass/FormStyle.module.scss";
-import { InputComponent } from "../../Input-component/Input-component";
-import { emailValidate, passwordValidate } from "../../Form-validation/FormValidate.component";
+import {
+  emailValidate,
+  passwordValidate,
+} from "../../Form-validation/FormValidate.component";
+import LoginBody from "./LoginBody.component";
 
 const Login = () => {
   type ValidateObject = {
@@ -51,49 +52,17 @@ const Login = () => {
       {isDataValidate ? (
         <Navigate to="/dashboard" replace={true} />
       ) : (
-        <FormWrapper
-          title="Login to "
-          firstFeature="Do you forgot your password ?"
-          firstFeatureLink="/form/reset"
-          secondFeature="Create new account"
-          secondFeatureLink="/form/signup"
-          buttonName="Login"
-        >
-          <div
-            className={
-              s.loginContainer__wrapper__loginFormContainer__form__inputsWrapper
-            }
-          >
-            <InputComponent
-              elementError={emailError}
-              labelTitle="Enter your email:"
-              inputType={"text"}
-              name={"email"}
-              id={"email"}
-              element={email}
-              setElement={setEmail}
-              hrefToElement={emailElement}
-            />
-            <InputComponent
-              elementError={passwordError}
-              labelTitle={"Enter your password:"}
-              inputType={"password"}
-              name={"password"}
-              id={"password"}
-              element={password}
-              setElement={setPassword}
-              hrefToElement={passwordElement}
-            />
-          </div>
-          <div className={s.button}>
-            <button
-              onClick={(e) => buttonSubmitHandler(e)}
-              className={s.button__btn_small}
-            >
-              Login
-            </button>
-          </div>
-        </FormWrapper>
+        <LoginBody
+          email={email}
+          emailElement={emailElement}
+          emailError={emailError}
+          setEmail={setEmail}
+          password={password}
+          passwordElement={passwordElement}
+          passwordError={passwordError}
+          setPassword={setPassword}
+          buttonSubmitHandler={buttonSubmitHandler}
+        />
       )}
     </>
   );
---
+++ frontend/src/Components/Form/Forms/Login/LoginBody.component.tsx
@@ -0,0 +1,76 @@
+import s from "../../Form-sass/FormStyle.module.scss";
+import { FormWrapper } from "../../FormWrapper-component/FormWrapper.component";
+import { InputComponent } from "../../Input-component/Input-component";
+import { type MouseEvent, type RefObject } from "react";
+
+type LoginBody = {
+  emailError: { isError: boolean; errorMessage: string | null };
+  email: string;
+  setEmail: (value: string) => void;
+  emailElement: RefObject<HTMLInputElement>;
+  passwordError: { isError: boolean; errorMessage: string | null  };
+  password: string;
+  setPassword: (value: string) => void;
+  passwordElement: RefObject<HTMLInputElement>;
+  buttonSubmitHandler: (e: MouseEvent<HTMLButtonElement>) => void;
+};
+
+const LoginBody = ({
+  emailError,
+  email,
+  setEmail,
+  emailElement,
+  password,
+  passwordElement,
+  passwordError,
+  setPassword,
+  buttonSubmitHandler
+}: LoginBody) => {
+  return (
+    <FormWrapper
+      title="Login to "
+      firstFeature="Do you forgot your password ?"
+      firstFeatureLink="/form/reset"
+      secondFeature="Create new account"
+      secondFeatureLink="/form/signup"
+      buttonName="Login"
+    >
+      <div
+        className={
+          s.loginContainer__wrapper__loginFormContainer__form__inputsWrapper
+        }
+      >
+        <InputComponent
+          elementError={emailError}
+          labelTitle="Enter your email:"
+          inputType={"text"}
+          name={"email"}
+          id={"email"}
+          element={email}
+          setElement={setEmail}
+          hrefToElement={emailElement}
+        />
+        <InputComponent
+          elementError={passwordError}
+          labelTitle={"Enter your password:"}
+          inputType={"password"}
+          name={"password"}
+          id={"password"}
+          element={password}
+          setElement={setPassword}
+          hrefToElement={passwordElement}
+        />
+      </div>
+      <div className={s.button}>
+        <button
+          onClick={(e) => buttonSubmitHandler(e)}
+          className={s.button__btn_small}
+        >
+          Login
+        </button>
+      </div>
+    </FormWrapper>
+  );
+};
+
+export default LoginBody;
