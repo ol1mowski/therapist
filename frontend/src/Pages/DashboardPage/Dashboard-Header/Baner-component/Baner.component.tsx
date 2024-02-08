a0d3b513eca41499bc73a2ce14411c1263a3b2de
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-08 11:40:09 +0100

    [Header] refactoring: splite header component

---
+++ frontend/src/Pages/DashboardPage/Dashboard-Header/Baner-component/Baner.component.tsx
@@ -0,0 +1,14 @@
+import s from "../../Dashboard-style/Dashboard.module.scss";
+
+
+const Baner = () => {
+  return (
+    <section className={s.headerContainer__projectTitle}>
+      <h2>
+        <span className={s.alice}>Alice</span> Virtual AI Therapist
+      </h2>
+    </section>
+  );
+};
+
+export default Baner;
