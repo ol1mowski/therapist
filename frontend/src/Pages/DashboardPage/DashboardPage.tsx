34d4ed13fb1798a9b6946b7600599d391b331bdc
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-14 17:36:03 +0100

    [Dashboard] style: UI update

--- frontend/src/Pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/Pages/DashboardPage/DashboardPage.tsx
@@ -1,18 +1,11 @@
 import s from "./Dashboard-style/Dashboard.module.scss";
 
-import DashboardHeader from "./Dashboard-Header/DashboardHeader";
-import DashboardHistory from "./Dashboard-History/DashboardHistory";
-import DashboardNews from "./Dashboard-News/DashboadNews";
 
 export const DashboardPage = () => {
   return (
     <>
       <section className={s.header}>
-        <DashboardHeader />
-      </section>
-      <section className={s.body}>
-        <DashboardHistory />
-        <DashboardNews />
+        <h1>In build ☺️</h1>
       </section>
     </>
   );
