c1ca5c5ff2110e9d0c9f7970db6b6460dcf140bb
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-18 17:30:10 +0100

    [DashboardPage] styel: add style to nav side

--- frontend/src/Pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/Pages/DashboardPage/DashboardPage.tsx
@@ -5,24 +5,49 @@ export const DashboardPage = () => {
     <>
       <section className={s.dashboardContainer}>
         <nav className={s.dashboardContainer__nav}>
-          <section className={s.dashboardContainer__nav__hamburger}>
-            <div
-              className={`${s.dashboardContainer__nav__hamburger__line} ${s.left}`}
-            ></div>
-            <div
-              className={`${s.dashboardContainer__nav__hamburger__line} ${s.center}`}
-            ></div>
-            <div
-              className={`${s.dashboardContainer__nav__hamburger__line} ${s.right}`}
-            ></div>
-          </section>
           <section className={s.dashboardContainer__nav__top}>
-            <div className={s.dashboardContainer__nav__item}></div>
-            <div className={s.dashboardContainer__nav__item}></div>
+            <div className={s.dashboardContainer__nav__item}>
+              <img
+                width="40"
+                height="40"
+                src="https://img.icons8.com/small/40/ffffff/home.png"
+                alt="home"
+              />
+            </div>
+            <div className={s.dashboardContainer__nav__item}>
+              <img
+                width="40"
+                height="40"
+                src="https://img.icons8.com/fluency-systems-regular/40/ffffff/guest-male.png"
+                alt="guest-male"
+              />
+            </div>
+            <div className={s.dashboardContainer__nav__item}>
+              <img
+                width="40"
+                height="40"
+                src="https://img.icons8.com/windows/40/ffffff/talk-female.png"
+                alt="talk-female"
+              />
+            </div>
           </section>
           <section className={s.dashboardContainer__nav__bottom}>
-            <div className={s.dashboardContainer__nav__item}></div>
-            <div className={s.dashboardContainer__nav__item}></div>
+            <div className={s.dashboardContainer__nav__item}>
+              <img
+                width="40"
+                height="40"
+                src="https://img.icons8.com/ios/40/ffffff/settings--v1.png"
+                alt="settings--v1"
+              />
+            </div>
+            <div className={s.dashboardContainer__nav__item}>
+              <img
+                width="35"
+                height="35"
+                src="https://img.icons8.com/ios/35/ffffff/exit--v1.png"
+                alt="exit--v1"
+              />
+            </div>
           </section>
         </nav>
         <main className={s.dashboardContainer__contentWrapper}>
