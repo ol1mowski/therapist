562df2e94863c97a95b369cb4d06347119cefdbe
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-29 21:52:36 +0100

    [AccountDashboardComponent] new feature: add show passowrd feature

--- frontend/src/Components/DashboardComponnents/Account-component/Account.component.tsx
+++ frontend/src/Components/DashboardComponnents/Account-component/Account.component.tsx
@@ -1,8 +1,30 @@
 import { Form } from "react-router-dom";
 
 import s from "../Dashboard-style/Dashboard.component.module.scss";
+import { useState } from "react";
 
 const Account = () => {
+  const imageSrc = [
+    "https://img.icons8.com/material-outlined/24/A68DDD/hide.png",
+    "https://img.icons8.com/material-outlined/24/A68DDD/visible--v1.png",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const [iconSrc, setIconSrc] = useState<string>(
+    "https://img.icons8.com/material-outlined/24/A68DDD/hide.png"
+  );
+
+  const changeIconHandler = () => {
+    if (currentIndex === 0) {
+      setCurrentIndex(1);
+      setIconSrc(imageSrc[1]);
+    } else {
+      setCurrentIndex(0);
+      setIconSrc(imageSrc[0]);
+    }
+  };
+
   return (
     <main className={s.accountContainer}>
       <header className={s.accountContainer__header}>
@@ -29,27 +51,39 @@ const Account = () => {
             </p>
           </div>
         </section>
-        <Form className={s.accountContainer__inputWrapper}>
+        <Form method="post" className={s.accountContainer__formWrapper}>
           <label
-            className={s.accountContainer__inputWrapper__label}
+            className={s.accountContainer__formWrapper__label}
             htmlFor={"password"}
           >
             Change your password
           </label>
-          <div
-            className={
-              s.loginContainer__wrapper__loginFormContainer__form__inputsWrapper__inputWrapper__wrapper
-            }
-          >
-            <input
-              className={s.accountContainer__inputWrapper__input}
-              type="password"
-              name="password"
+          <div className={s.accountContainer__formWrapper__inputWrapper}>
+            {currentIndex === 1 ? (
+              <input
+                className={s.accountContainer__formWrapper__inputWrapper__input}
+                type="text"
+                name="password"
+              />
+            ) : (
+              <input
+                className={s.accountContainer__formWrapper__inputWrapper__input}
+                type="password"
+                name="password"
+              />
+            )}
+
+            <img
+              onClick={changeIconHandler}
+              src={iconSrc}
+              alt="passoword icon"
+              className={
+                s.accountContainer__formWrapper__inputWrapper__inputIcon
+              }
             />
           </div>
-          <div  className={s.accountContainer__button}>
-
-          <button className={s.accountContainer__button__btn}>Change</button>
+          <div className={s.accountContainer__button}>
+            <button className={s.accountContainer__button__btn}>Change</button>
           </div>
         </Form>
       </section>
