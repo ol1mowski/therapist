da27c4c01b12aee736c9c45aa712443a53c4b694
Ol1mowski <101707516+ol1mowski@users.noreply.github.com>
2024-02-29 09:15:07 +0100

    [AccountComponent] sketch: add simple HTML sketch to component and some styles

--- frontend/src/Components/DashboardComponnents/Account-component/Account.component.tsx
+++ frontend/src/Components/DashboardComponnents/Account-component/Account.component.tsx
@@ -1,16 +1,37 @@
 import { Form } from "react-router-dom";
 
-import s from "../../../Components/Form/Form-sass/FormStyle.module.scss";
+import s from "../Dashboard-style/Dashboard.component.module.scss";
 
 const Account = () => {
   return (
-    <main>
-      <Form>
-        <section>
+    <main className={s.accountContainer}>
+      <header className={s.accountContainer__header}>
+        <h2 className={s.accountContainer__header__title}>Change Your Data</h2>
+      </header>
+      <section className={s.accountContainer__dataWrapper}>
+        <section className={s.accountContainer__dataWrapper__topSide}>
+          <div className={s.accountContainer__dataWrapper__topSide__item}>
+            <p
+              className={
+                s.accountContainer__dataWrapper__topSide__item__content
+              }
+            >
+              Your name: Alice
+            </p>
+          </div>
+          <div className={s.accountContainer__dataWrapper__topSide__item}>
+            <p
+              className={
+                s.accountContainer__dataWrapper__topSide__item__content
+              }
+            >
+              Your email: alice@alice.pl
+            </p>
+          </div>
+        </section>
+        <Form className={s.accountContainer__inputWrapper}>
           <label
-            className={
-              s.loginContainer__wrapper__loginFormContainer__form__inputsWrapper__inputWrapper__label
-            }
+            className={s.accountContainer__inputWrapper__label}
             htmlFor={"password"}
           >
             Change your password
@@ -21,16 +42,17 @@ const Account = () => {
             }
           >
             <input
-              className={
-                s.loginContainer__wrapper__loginFormContainer__form__inputsWrapper__inputWrapper__input
-              }
+              className={s.accountContainer__inputWrapper__input}
               type="password"
               name="password"
             />
           </div>
-          <button className={s.button__btn_small}>Change</button>
-        </section>
-      </Form>
+          <div  className={s.accountContainer__button}>
+
+          <button className={s.accountContainer__button__btn}>Change</button>
+          </div>
+        </Form>
+      </section>
     </main>
   );
 };
